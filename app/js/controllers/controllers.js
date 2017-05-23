2622889f6db7dde3b101b03b43b189f4e91d127a
Bharat Patil <patilbharat09@gmail.com>
2017-05-23 10:33:59 +0530

    Corrected indentation

--- app/js/controllers/controllers.js
+++ app/js/controllers/controllers.js
@@ -1,5 +1,8 @@
-define(['angular', 'controllers/View1Controller', 'controllers/View2Controller'], (angular,         View1Controller, View2Controller) => {
+define(['angular', 'controllers/View1Controller', 'controllers/View2Controller'],
+(angular,View1Controller, View2Controller) => {
+
   'use strict';
+
   var controllers = angular.module('App.controllers', []);
   controllers.controller('View1Controller', View1Controller);
   controllers.controller('View2Controller', View2Controller);
